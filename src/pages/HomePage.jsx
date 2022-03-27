15fba512e968cea18d5ade9a2a7d6bc83433bf1a
Ibnul Ashir <ibnul.mistrisolutions@gmail.com>
2022-03-27 17:46:46 +0600

    components update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Authentication from '../components/Authentication'
-import Header from '../components/Header'
-import InfoContact from '../components/InfoContact'
+import React from "react";
+import Authentication from "../components/Authentication";
+import Header from "../components/Header";
+import InfoContact from "../components/InfoContact";
 
 function HomePage() {
   return (
-    <div className="h-fit w-fit flex flex-col overflow-hidden">
-        <div className="px-10 md:px-20 lg:px-32 flex flex-col">
-            <Header />
-            <Authentication />
-            {/* <InfoContact /> */}
-        </div>
+    <div className="h-fit w-screen flex flex-col overflow-hidden">
+      <div className="px-10 md:px-20 lg:px-32 flex flex-col">
+        <Header />
+        <Authentication />
+        {/* <InfoContact /> */}
+      </div>
     </div>
-  )
+  );
 }
 
 export default HomePage;
