719a3ec494f192f1fdca54a280c4677873e1c62c
Ibnul Ashir <ibnul.mistrisolutions@gmail.com>
2022-03-28 12:08:34 +0600

    validation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'react-phone-input-2/lib/style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "react-phone-input-2/lib/style.css";
+import "react-toastify/dist/ReactToastify.css";
+// import "react-toastify/dist/ReactToastify.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
